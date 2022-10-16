4b645652b5022ab0c90bfd6a722e42060184f203
no-self-required <louism.coding@gmail.com>
2022-10-15 21:35:02 -0400

    add main page and styling

---
+++ src/Components/Main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../Styling/Main.css";
+
+export default function Main() {
+  return (
+    <div id="container">
+      <div id="name">Louis Manlangit</div> 
+    </div>
+  );
+}
